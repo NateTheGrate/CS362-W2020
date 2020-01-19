a2ad7e46f3b503dc7828fce893ed1cffc4027b85
natethegrate <njbutler99@gmail.com>
2020-01-18 16:17:53 -0800

    refactored playDominion by putting utility functions into the testUtility class, and added 2 test scenario classes that introduced bugs

---
+++ dominion/testDominion1.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 18 14:52:42 2020
+
+@author: Nathanael Butler
+"""
+
+import Dominion
+import random
+import testUtility
+from collections import defaultdict
+
+#Get player names
+player_names = testUtility.createPlayers("Annie", "Ben", "Carla", "Bob", "yyvet", "abcccc", "cool")
+
+nV = testUtility.numVictoryCards(len(player_names))
+nC = testUtility.numCurseCards(len(player_names))
+
+#Define box
+box = testUtility.getBoxes(nV)
+
+supply_order = testUtility.getSupplyOrder()
+
+supply = testUtility.setupSupply(box, len(player_names), nV, nC)
+
+#initialize the trash
+trash = []
+
+#Costruct the Player objects
+players = testUtility.initializePlayers(player_names)
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1
+    print("\r")
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))    
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+            
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
